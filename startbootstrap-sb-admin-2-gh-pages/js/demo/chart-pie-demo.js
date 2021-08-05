ddbcfa0caba49a4bd67dba0780839d3c2fae9c36
Antara P <63465565+antarapal235@users.noreply.github.com>
2021-08-05 18:58:38 -0400

    finished basics of business dashboard

--- startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
+++ startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
@@ -7,7 +7,7 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Pfizer-BioNTech", "Moderna", "Johnson & Johnson"],
     datasets: [{
       data: [55, 30, 15],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
